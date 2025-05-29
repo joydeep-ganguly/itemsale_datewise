5f9333aa9d92e8a97ad5ee55d29183790a59e000
Joydeep Ganguly <joydeepganguly32@gmail.com>
2025-05-29 18:59:37 +0530

    Dataframe name change

--- app.py
+++ app.py
@@ -18,37 +18,38 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        form_inputs = pd.DataFrame(request.form.to_dict(),index=[0])
-        #form_inputs['area'] = form_inputs['area'].apply(lambda x : '24 PARGANAS(S)' if x == '24PGS(S)' else x)
-        #form_inputs['area'] = form_inputs['area'].apply(lambda x : '24 PARGANAS(N)' if x == 'NORTH 24 PGS' else x)
-        #form_inputs = form_inputs[(form_inputs['area'] != '-') & (form_inputs['area'] != '')]
+        form_itemsale = pd.DataFrame(request.form.to_dict(),index=[0])
 
-        #form_inputs['area'] = form_inputs['area'].apply(lambda x : area_encoder[x])
-        form_inputs.itemcode = form_inputs.itemcode.apply(lambda x : item_encoder[x])
+        #form_itemsale['area'] = form_itemsale['area'].apply(lambda x : '24 PARGANAS(S)' if x == '24PGS(S)' else x)
+        #form_itemsale['area'] = form_itemsale['area'].apply(lambda x : '24 PARGANAS(N)' if x == 'NORTH 24 PGS' else x)
+        #form_itemsale = form_itemsale[(form_itemsale['area'] != '-') & (form_itemsale['area'] != '')]
 
-        form_inputs['ddate'] = pd.to_datetime(form_inputs.ddate, format='%d-%m-%Y')
-        form_inputs['day'] = form_inputs.ddate.dt.day
-        form_inputs['month'] = form_inputs.ddate.dt.month
-        form_inputs['year'] = form_inputs.ddate.dt.year
-        form_inputs['weekday'] = form_inputs.ddate.dt.weekday
-        form_inputs['weekend'] = form_inputs.weekday.apply(lambda x: 1 if x > 4 else 0)
+        form_itemsale['area'] = form_itemsale['area'].apply(lambda x : area_encoder[x])
+        form_itemsale['itemcode'] = form_itemsale['itemcode'].apply(lambda x : item_encoder[x])
+
+        form_itemsale['ddate'] = pd.to_datetime(form_itemsale.ddate, format='%d-%m-%Y')
+        form_itemsale['day'] = form_itemsale.ddate.dt.day
+        form_itemsale['month'] = form_itemsale.ddate.dt.month
+        form_itemsale['year'] = form_itemsale.ddate.dt.year
+        form_itemsale['weekday'] = form_itemsale.ddate.dt.weekday
+        form_itemsale['weekend'] = form_itemsale.weekday.apply(lambda x: 1 if x > 4 else 0)
 
         festival['ddate'] =  pd.to_datetime(festival.ddate, format='%d-%m-%Y')
         festival['festival'] = 1
-        form_inputs = pd.merge(left=form_inputs,right=festival,left_on='ddate',right_on='ddate',how='left')
-        form_inputs.festival = form_inputs.festival.fillna(0)
+        form_itemsale = pd.merge(left=form_itemsale,right=festival,left_on='ddate',right_on='ddate',how='left')
+        form_itemsale.festival = form_itemsale.festival.fillna(0)
 
-        form_inputs['c1'] = np.sin(form_inputs.month * (2 * np.pi / 12))
-        form_inputs['c2'] = np.cos(form_inputs.month * (2 * np.pi / 12))
-        form_inputs.drop('ddate', axis=1, inplace=True)
-        features = form_inputs.drop(['year'],axis=1)
+        form_itemsale['c1'] = np.sin(form_itemsale.month * (2 * np.pi / 12))
+        form_itemsale['c2'] = np.cos(form_itemsale.month * (2 * np.pi / 12))
+        form_itemsale.drop('ddate', axis=1, inplace=True)
+        features = form_itemsale.drop(['year'],axis=1)
 
         X = scaler.transform(features)
         X = pd.DataFrame(X)
         X.columns = features.columns
 
         prediction = model.predict(X)
-        #return str(np.round(prediction[0]))
+        return str(np.round(prediction[0]))
 
 if __name__ == '__main__':
     app.run(debug=True)

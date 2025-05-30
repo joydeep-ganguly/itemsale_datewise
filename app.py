50adb3fecc02f0a1651ca733eccf0d7934592955
joydeep ganguly <joydeepganguly32@gmail.com>
2025-05-30 16:03:05 +0530

    code changed

--- app.py
+++ app.py
@@ -47,10 +47,10 @@ def index():
         X = scaler.transform(features)
         X = pd.DataFrame(X)
         X.columns = features.columns
-        return X.to_html()
+        #return X.to_html()
 
         prediction = model.predict(X)
-        #return str(np.round(prediction[0]))
+        return str(np.round(prediction[0]))
 
 if __name__ == '__main__':
     app.run(debug=True)
